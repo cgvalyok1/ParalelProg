f27cecf8933a340ea1f6cf7f13f5860e334f62a9
CGValyok <75014797+cgvalyok1@users.noreply.github.com>
2025-01-17 01:34:44 +1000

    new version with gen

--- parallel.cpp
+++ parallel.cpp
@@ -14,11 +14,30 @@
 void to_csv(std::ostream& io, std::vector<scalability_result> v) {
 	io << "N,Result,Time,Speedup,Efficiency\n";
 	for (unsigned i = 0; i < v.size(); i++) {
-		io << i << ',' << v.at(i).result << ',' << v.at(i).t << ',' << v.at(i).s << ',' << v.at(i).e << '\n';
+		io << i+1 << ',' << v.at(i).result << ',' << v.at(i).t << ',' << v.at(i).s << ',' << v.at(i).e << '\n';
 	}
 }
 
-int main() {	
+void to_csv_rand_result(std::ostream& io, std::vector<scalability_result> v) {
+	io << "N,Time,Speedup,Efficiency\n";
+	for (unsigned i = 0; i < v.size(); i++) {
+		io << i+1 << ',' << v.at(i).t << ',' << v.at(i).s << ',' << v.at(i).e << '\n';
+	}
+}
+
+int main() {
+	
+	//randomize experiment
+	unsigned sz = 2 << 18; // 2^18 elements in array
+	auto randarr = run_experiment_rand(randomize, sz, 0, 1000, 0);
+	std::ofstream osrandom("randomize_experiment.csv", std::ios_base::out);
+	if (osrandom.is_open()) {
+		to_csv_rand_result(osrandom, randarr);
+		osrandom.close();
+	}
+	
+	
+	/*
 	auto sr = run_experiment(sum, 1u, 1u << 25);
 	std::ofstream os("sum.csv", std::ios_base::out);
 	if (os.is_open()) {
@@ -74,7 +93,7 @@ int main() {
 		to_csv(os7, sr7);
 		os7.close();
 	}
-
+	*/
 	/*unsigned* vect = new unsigned[1000000];
 	for (unsigned i = 0, j = 0; i < 1000000; i++, j++) {
 		vect[j] = i;
